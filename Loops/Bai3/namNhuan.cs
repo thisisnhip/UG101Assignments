fd1434d218bd9da6a522f2f49adf217b6e46b03a
thisisnhip <nhinguyenphuong2005@gmail.com>
2024-10-09 23:34:24 +0700

    Create namNhuan.cs

---
+++ Loops/Bai3/namNhuan.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+            int year;
+            bool namNhuan = false;
+            Console.Write("Nhap nam: ");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        namNhuan = true;
+                    }
+                }
+                else
+                {
+                    namNhuan = true;
+                }
+            }
+
+            if (namNhuan)
+            {
+                Console.WriteLine($"{year} la nam nhuan");
+            }
+            else
+            {
+                Console.WriteLine($"{year} khong phai nam nhuan");
+            }
+    }
+}
